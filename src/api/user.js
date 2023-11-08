aaee6dfa5825eef6e6d06cf1c8ea3daa44689b4f
Ayawow <1123325454@qq.com>
2023-11-08 08:58:26 +0800

    second commit

---
+++ src/api/user.js
@@ -0,0 +1,11 @@
+import instance from '@/utils/request.js'
+
+//注册接口
+export const userRegisterService = ({ username, password, repassword }) => {
+  return instance.post('/api/reg', { username, password, repassword })
+}
+
+//登录
+export const userLoginService = ({ username, password }) => {
+  return instance.post('/api/login', { username, password })
+}
